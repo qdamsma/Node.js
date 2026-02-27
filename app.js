eb23d45307ce2236256da63d158fac7f0769422b
Quinten <90333974+qdamsma@users.noreply.github.com>
2026-02-27 14:21:45 +0100

    Rauwe logica van requests refresher

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    const url = req.url;
+    const method = req.method;
+    // Handle GET request to root URL
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Home Page</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message" placeholder="Enter message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    // Handle POST request to /message
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('messages.txt', message);
+        });
+        res.writeHead(302, { 'Location': '/' });
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>Hello World</h1>');
+    res.end();
 });
 
 server.listen(3000);
