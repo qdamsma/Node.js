736c034a26376dcd61515f66fbed39f1611f36d6
Quinten <90333974+qdamsma@users.noreply.github.com>
2026-03-09 16:11:46 +0100

    ejs templates

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const bodyParser = require('body-parser');
 
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(shopRoutes);
-app.use('/admin', adminRoutes);
-
 app.use(express.static('public'));
 
+app.use(shopRoutes);
+app.use('/admin', adminData.routes);
+
+
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);   
--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,12 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../helper/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop', {prods: adminData.products, pageTitle: 'Shop', path: '/'});
 });
 
 module.exports = router;
