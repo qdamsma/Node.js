1ac4b10547436183f68e6c3cdbbb229ccd1b84bd
Quinten <90333974+qdamsma@users.noreply.github.com>
2026-03-09 14:38:22 +0100

    scss implementatie

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
@@ -10,8 +11,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(shopRoutes);
 app.use('/admin', adminRoutes);
 
+app.use(express.static('public'));
+
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page Not Found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);   
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,12 @@
+const path = require('path');
 const express = require('express');
+
+const rootDir = require('../helper/path');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('<h1>Welcome to the Shop!</h1>');
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
