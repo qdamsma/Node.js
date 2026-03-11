ccafe7442782397319b3a7f88785d2771ed87c4b
Quinten <90333974+qdamsma@users.noreply.github.com>
2026-03-11 15:42:23 +0100

    Structuur veranderingen

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,13 +8,17 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const description = req.body.description;
+    const price = req.body.price;
+    const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
-        res.render('shop/product-list', { prods: products, pageTitle: 'Shop', path: '/' });
+        res.render('admin/products', { prods: products, pageTitle: 'Admin Products', path: '/admin/products' });
     });
 };
