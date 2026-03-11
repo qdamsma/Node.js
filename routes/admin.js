503ebcba0207f060cd4f5df0ec8bead4a72957c6
Quinten <90333974+qdamsma@users.noreply.github.com>
2026-03-11 14:48:44 +0100

    MVC setup

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,14 @@
 const path = require('path');
 const express = require('express');
 
-const rootDir = require('../helper/path');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
+router.get('/add-product', productsController.getAddProduct);
 
-router.get('/add-product',(req, res, next) => {
-  res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
-});
+router.get('/products');
 
-router.post('/add-product',(req, res, next) => {
-  products.push({title: req.body.title});
-  res.redirect('/');  
-});
+router.post('/add-product', productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,16 @@
 const path = require('path');
 const express = require('express');
 
-const rootDir = require('../helper/path');
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.render('shop', {prods: adminData.products, pageTitle: 'Shop', path: '/'});
-});
+router.get('/', productsController.getProducts);
+
+router.get('/products');
+
+router.get('/cart');
+
+router.get('/checkout');
 
 module.exports = router;
