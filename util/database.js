5e8f98048f492f7d82cf492166687cc9c9b564a4
Quinten <90333974+qdamsma@users.noreply.github.com>
2026-03-16 17:32:40 +0100

    sequelize

--- util/database.js
+++ util/database.js
@@ -1,11 +1,6 @@
 require('dotenv').config();
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD
-});
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {dialect: process.env.DB_DIALECT, host: process.env.DB_HOST});
 
-module.exports = pool.promise();
+module.exports = sequelize;
