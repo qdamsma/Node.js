022ab43f33c704757f504444c59fe253db7cd38a
Quinten <90333974+qdamsma@users.noreply.github.com>
2026-03-16 17:11:19 +0100

    db instert met veiligheid voor sql

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,9 +2,9 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render('shop/product-list', { prods: products, pageTitle: 'All products', path: '/products' });
-    });
+    Product.fetchAll().then(([rows, fieldData]) => {
+        res.render('shop/product-list', { prods: rows, pageTitle: 'All products', path: '/products' });
+    }).catch(err => console.log(err));
 };
 
 exports.getProduct = (req, res, next) => {
@@ -15,9 +15,9 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render('shop/index', { prods: products, pageTitle: 'Shop', path: '/' });
-    });
+    Product.fetchAll().then(([rows, fieldData]) => {
+        res.render('shop/index', { prods: rows, pageTitle: 'Shop', path: '/' });
+    }).catch(err => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
