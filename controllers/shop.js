49c0ba55535a044d7434f38f11f4b7268fc82f95
Quinten <90333974+qdamsma@users.noreply.github.com>
2026-03-22 15:01:37 +0100

    Orders toegevoegd na cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
     Product.findAll().then(products => {
@@ -63,16 +62,49 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findById(prodId, product => {
-        Cart.deleteProduct(prodId, product.price);
-        res.redirect('/cart');
-    });
+    req.user.getCart()
+        .then(cart => {
+            return cart.getProducts({ where: { id: prodId } });
+        })
+        .then(products => {
+            const product = products[0];
+            return product.cartItem.destroy();
+        })
+        .then(result => {
+            res.redirect('/cart');
+        })
+        .catch(err => console.log(err));
+};
+
+exports.postOrder = (req, res, next) => {
+    let fetchedCart;
+    req.user.getCart().then(cart => {
+        fetchedCart = cart;
+        return cart.getProducts();
+    })
+    .then(products => {
+        return req.user.createOrder()
+        .then(order => {
+            return order.addProducts(products.map(product => {
+                product.orderItem = { quantity: product.cartItem.quantity};
+                return product
+            }));
+        })
+        .catch(err => console.log(err));
+    })
+    .then(result => {
+        fetchedCart.setProducts(null);
+    })
+    .then(result => {
+        res.redirect('/orders');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-    res.render('shop/orders', { pageTitle: 'Your Orders', path: '/orders' });
-};
-
-exports.getCheckout = (req, res, next) => {
-    res.render('shop/checkout', { pageTitle: 'Checkout', path: '/checkout' });
+    req.user.getOrders({include: ['products']})
+    .then(orders => {
+        res.render('shop/orders', { pageTitle: 'Your Orders', path: '/orders', orders: orders });
+    })
+    .catch(err => console.log(err));
 };
