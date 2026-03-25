5a964867becc9fa1e63d28813817c7086ce91280
Quinten <90333974+qdamsma@users.noreply.github.com>
2026-03-26 00:20:36 +0100

    authenticatie begin

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,7 +4,8 @@ exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
-        editing: false
+        editing: false, 
+        isAuthenticated: req.isLoggedIn
     });
 };
 
@@ -38,7 +39,8 @@ exports.getEditProduct = (req, res, next) => {
                 pageTitle: 'Edit Product',
                 path: '/admin/edit-product',
                 editing: editMode,
-                product: product
+                product: product, 
+                isAuthenticated: req.isLoggedIn
             });
         }).catch(err => console.log(err));
 };
@@ -68,7 +70,7 @@ exports.postEditProduct = (req, res, next) => {
 exports.getProducts = (req, res, next) => {
     Product.find()
         .then(products => {
-            res.render('admin/products', { prods: products, pageTitle: 'Admin Products', path: '/admin/products' });
+            res.render('admin/products', { prods: products, pageTitle: 'Admin Products', path: '/admin/products', isAuthenticated: req.isLoggedIn });
         })
         .catch(err => console.log(err));
 };
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,19 @@
 exports.getLogin = (req, res, next) => {
-    res.render('auth/login', { pageTitle: 'Login', path: '/login'});
+    const cookieHeader = req.get('Cookie') || '';
+    const loggedInCookie = cookieHeader
+        .split(';')
+        .find(c => c.trim().startsWith('loggedIn='));
+    const isLoggedIn = loggedInCookie?.split('=')[1] === 'true';
+
+    res.render('auth/login', { 
+        pageTitle: 'Login', 
+        path: '/login', 
+        isAuthenticated: isLoggedIn 
+    });
+};
+
+exports.postLogin = (req, res, next) => {
+    res.setHeader('Set-Cookie', 'loggedIn=true')
+    res.redirect('/');
 
 };
