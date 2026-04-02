58b47f10419fa698102cee4f4015395fcb592ac3
Quinten <90333974+qdamsma@users.noreply.github.com>
2026-04-02 14:27:26 +0200

    validation login sign in en sanatizing

--- routes/auth.js
+++ routes/auth.js
@@ -10,28 +10,35 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post('/login',
+  [
+    check('email').isEmail().withMessage('Please enter a valid email address.').normalizeEmail(),
+    check('password', 'Password has to be valid.').isLength({ min: 5 }).isAlphanumeric().trim()
+  ],
+  authController.postLogin
+);
 
 router.post('/signup',
     [
         // Validatie voor signup
         check('email').isEmail().withMessage('Please enter a valid email').custom((value, { req }) => {
-            return User.findOne({ email: email })
+            return User.findOne({ email: value })
                 .then(userDoc => {
                     if (userDoc) {
                         return Promise.reject('E-Mail exists already, please pick a different one.');
                     }
                 });
-        }),
-        check('password', 'Please enter a password with only numbers and text and at least 5 characters.').isLength({ min: 5 }).isAlphanumeric(),
-        check('confirmPassword').custom((value, { req }) => {
+        }).normalizeEmail(),
+        check('password', 'Please enter a password with only numbers and text and at least 5 characters.').isLength({ min: 5 }).isAlphanumeric().trim(),
+        check('confirmPassword').trim().custom((value, { req }) => {
             if (value !== req.body.password) {
                 throw new Error('Passwords have to match!');
             }
             return true;
         })
     ],
-    authController.postSignup);
+    authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
