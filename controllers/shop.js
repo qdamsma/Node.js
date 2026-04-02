0e22b411bb1abfbab17afe8e1b2c56121b82224a
Quinten <90333974+qdamsma@users.noreply.github.com>
2026-04-02 19:47:56 +0200

    paginatie

--- controllers/shop.js
+++ controllers/shop.js
@@ -7,10 +7,32 @@ const Product = require('../models/product');
 const Order = require('../models/order');
 const mongoose = require('mongoose');
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
-    Product.find().then(products => {
-        res.render('shop/product-list', { prods: products, pageTitle: 'All products', path: '/products' });
+    const page = +req.query.page || 1;
+    let totalItems;
+
+    Product.find().countDocuments().then(numProducts => {
+        totalItems = numProducts;
+        return Product.find()
+            .skip((page - 1) * ITEMS_PER_PAGE)
+            .limit(ITEMS_PER_PAGE);
     })
+        .then(products => {
+            res.render('shop/product-list', {
+                prods: products,
+                pageTitle: 'Products',
+                path: '/products',
+                // Paginatie informatie
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+            });
+        })
         .catch(err => {
             const error = new Error(err);
             error.httpStatusCode = 500;
@@ -33,9 +55,29 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-    Product.find().then(products => {
-        res.render('shop/index', { prods: products, pageTitle: 'Shop', path: '/' });
+    const page = +req.query.page || 1;
+    let totalItems;
+
+    Product.find().countDocuments().then(numProducts => {
+        totalItems = numProducts;
+        return Product.find()
+            .skip((page - 1) * ITEMS_PER_PAGE)
+            .limit(ITEMS_PER_PAGE);
     })
+        .then(products => {
+            res.render('shop/index', {
+                prods: products,
+                pageTitle: 'Shop',
+                path: '/',
+                // Paginatie informatie
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+            });
+        })
         .catch(err => {
             const error = new Error(err);
             error.httpStatusCode = 500;
